e9c0f503fcc97d529af82653d4d32c3966482567
Abderraouf Gaouer <gaouerraouf02@gmail.com>
2024-05-01 15:05:45 +0100

    Started working on enemy animator

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float health = 100f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
+}
